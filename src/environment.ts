e51ce3df6e0cee1ec851804b39f2cf6f5f76a458
Szymon Kubiak <skub@thaumatec.com>
2024-06-20 19:04:15 +0200

    fixes

--- src/environment.ts
+++ src/environment.ts
@@ -1,12 +1,5 @@
-const prod = {
-  BACKEND_HOST: window.location.protocol + "//" + window.location.host,
-  SOCKET_HOST: "wss://" + window.location.host + ":81",
-};
-
 const dev = {
-  BACKEND_HOST:
-    window.location.protocol + "//" + window.location.hostname + ":3001",
-  SOCKET_HOST: "wss://" + window.location.hostname + ":81",
+  BACKEND_HOST: window.location.origin,
 };
 
 export const environment = dev;
